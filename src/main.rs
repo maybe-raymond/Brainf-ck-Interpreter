a3cc13a1ba0fbfc2ebe54b46852c37c05f3a0cf6
ray-aura <raymond@auragrp.com>
2026-03-11 16:00:20 +0200

    Add some comments and formatted the code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::stdin;
 
 fn skip_loop(buffer: &Vec<u8>, mut current_instruction: usize) -> usize {
     // for situations in which [ command is used but pointer is zero
+    // Will skip all instructions until it finds ] command
     loop {
         let Some(token) = buffer.get(current_instruction) else {
             break;
@@ -13,6 +14,7 @@ fn skip_loop(buffer: &Vec<u8>, mut current_instruction: usize) -> usize {
             current_instruction += 1;
             break;
         } else if *token == b'[' {
+            // for nested loops
             current_instruction += 1;
             current_instruction = skip_loop(&buffer, current_instruction);
         }
@@ -24,10 +26,9 @@ fn skip_loop(buffer: &Vec<u8>, mut current_instruction: usize) -> usize {
 }
 
 fn interpreter(mut file: File) {
+    const CAPACITY: usize = 16;
 
-    const CAPACITY:usize = 16; 
-
-    let mut array: [u8; CAPACITY] = [0; CAPACITY]; // memory
+    let mut memory: [u8; CAPACITY] = [0; CAPACITY]; // memory
     let mut stack = Vec::new(); // to house the instruction to pointer of [ 
 
     let mut current_instruction = 0;
@@ -39,7 +40,8 @@ fn interpreter(mut file: File) {
     file.read_to_end(&mut buffer)
         .expect("could not read from file");
 
-    let acceptable_symbols = [43, 44, 45, 46, 60, 62, 91, 93]; // filter everything that is not in the language
+    let acceptable_symbols = [43, 44, 45, 46, 60, 62, 91, 93];
+    // filter everything that is not in the language
     let buffer: Vec<u8> = buffer
         .into_iter()
         .filter(|x| acceptable_symbols.contains(x))
@@ -48,7 +50,7 @@ fn interpreter(mut file: File) {
     loop {
         let Some(token) = buffer.get(current_instruction) else {
             println!();
-            println!("{array:?}");
+            println!("{memory:?}");
             break;
         };
 
@@ -63,15 +65,15 @@ fn interpreter(mut file: File) {
                 current_instruction += 1;
             }
             b'+' => {
-                array[current_memory_pointer] = array[current_memory_pointer] + 1;
+                memory[current_memory_pointer] = memory[current_memory_pointer] + 1;
                 current_instruction += 1;
             }
             b'-' => {
-                array[current_memory_pointer] = array[current_memory_pointer] - 1;
+                memory[current_memory_pointer] = memory[current_memory_pointer] - 1;
                 current_instruction += 1;
             }
             b'[' => {
-                if array[current_memory_pointer] != 0 {
+                if memory[current_memory_pointer] != 0 {
                     stack.push(current_instruction);
                     current_instruction += 1;
                 } else {
@@ -80,7 +82,7 @@ fn interpreter(mut file: File) {
                 }
             }
             b']' => {
-                if array[current_memory_pointer] != 0 {
+                if memory[current_memory_pointer] != 0 {
                     current_instruction = stack
                         .pop()
                         .expect("stack error happened. need to add debuging for later");
@@ -89,7 +91,7 @@ fn interpreter(mut file: File) {
                 }
             }
             b'.' => {
-                print!("{}", array[current_memory_pointer] as char); // it is buffered and will only flush when full
+                print!("{}", memory[current_memory_pointer] as char); // it is buffered and will only flush when full
                 current_instruction += 1;
             }
 
@@ -103,7 +105,7 @@ fn interpreter(mut file: File) {
                     .parse::<u8>()
                     .expect("input cannot be converted to a number");
 
-                array[current_memory_pointer] = number;
+                memory[current_memory_pointer] = number;
                 current_instruction += 1;
             }
             _ => {
@@ -121,10 +123,8 @@ fn main() {
         return;
     }
 
-    match File::open(&args[1]){
+    match File::open(&args[1]) {
         Ok(file) => interpreter(file),
-        Err(_) => println!("Could not open file with name {:?}", &args[1])
-
+        Err(_) => println!("Could not open file with name {:?}", &args[1]),
     };
-
-    }
+}
